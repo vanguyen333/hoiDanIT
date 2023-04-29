a9cac64bcddb0d2906a4efdc06e905787ffbddc3
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2023-04-28 22:13:52 -0400

    numbers

---
+++ file.js
@@ -0,0 +1,8 @@
+console.log("Hello world from Green Pear!".toUpperCase());
+let a = 5;
+let b = 6;
+let c = 6.9;
+let d = "5";
+console.log(a + b); //11
+console.log(a + c); //11.9
+console.log(a === d, a, typeof a, d, typeof d);
