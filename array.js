edbbcb09812a828e5838213f5067ea563fabbf91
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2023-05-01 17:43:55 -0400

    added array of objects

--- array.js
+++ array.js
@@ -1,3 +1,9 @@
 console.log("Learn array!");
 let b = ["MU", "Chelsea", "Liver"];
 console.log(`typeof b:`, typeof b, b);
+console.log(`first item: `, b[0]);
+let c = [
+  { name: "Eric", age: 25 },
+  { name: "Nali", age: 23 },
+];
+console.log(c);
