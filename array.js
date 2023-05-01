ee6814e2e066ed9f557e7c8a2a5a290cdd0280fb
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2023-05-01 18:42:36 -0400

    added for loop file

--- array.js
+++ array.js
@@ -1,9 +1,12 @@
-console.log("Learn array!");
-let b = ["MU", "Chelsea", "Liver"];
-console.log(`typeof b:`, typeof b, b);
-console.log(`first item: `, b[0]);
-let c = [
-  { name: "Eric", age: 25 },
-  { name: "Nali", age: 23 },
-];
-console.log(c);
+//learn array
+// console.log("Learn array!");
+// let b = ["MU", "Chelsea", "Liver"];
+// console.log(`typeof b:`, typeof b, b);
+// console.log(`first item: `, b[0]);
+// let c = [
+//   { name: "Eric", age: 25 },
+//   { name: "Nali", age: 23 },
+// ];
+// console.log(c);
+
+//learn comparison
