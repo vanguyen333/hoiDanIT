1e998e6f6c3b74e6da6de516eaa499f5243424e2
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2023-05-01 16:59:14 -0400

    change to file.js, console.log with object value

--- file.js
+++ file.js
@@ -1,8 +1,16 @@
 console.log("Hello world from Green Pear!".toUpperCase());
-let a = 5;
-let b = 6;
-let c = 6.9;
-let d = "5";
-console.log(a + b); //11
-console.log(a + c); //11.9
-console.log(a === d, a, typeof a, d, typeof d);
+// let a = 5;
+// let b = 6;
+// let c = 6.9;
+// let d = "5";
+// console.log(a + b); //11
+// console.log(a + c); //11.9
+// console.log(a === d, a, typeof a, d, typeof d);
+
+// Video 8 Kieu du lieu Object
+let obj = {
+  name: "Eric",
+  address: "Hanoi",
+};
+let b = "Eric";
+console.log(`What is your name? My name is: ${obj.name}`);
