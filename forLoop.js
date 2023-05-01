d3a6511a1225749a3bf24c116861a629b962fd53
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2023-05-01 19:25:38 -0400

    added while loop

--- forLoop.js
+++ forLoop.js
@@ -6,6 +6,12 @@ console.log("Learn For Loop!");
 // }
 
 let arr = ["MU", "ManCity", "Liver", "Chelsea"];
-for (let i = 0; i < arr.length; i++) {
-  console.log(`Top: `, i + 1, arr[i]);
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(`Top`, i + 1, arr[i]);
+// }
+
+var i = 0;
+while (i < 4) {
+  console.log(`Top`, i + 1, arr[i]);
+  i++;
 }
