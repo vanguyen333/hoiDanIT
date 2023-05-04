78fa12ecc7162b27ebc616b5f451fe9c7c6052c5
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2023-05-03 21:39:19 -0400

    ...

--- functions.js
+++ functions.js
@@ -1,5 +1,13 @@
 console.log("Let's learn functions in javaScript!");
 
-function sum(a, b) {
+//parameter: tham so dau vao
+// function sum(a, b) {
+//   return a + b;
+// }
+// let c = sum(3, 7);
+// console.log(`sum a + b:`, c);
+
+let sum = (a, b) => {
   return a + b;
-}
+};
+console.log(`check sum of 3 and 9`, sum(3, 9));
